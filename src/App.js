048b2b454b831302b0ab02b6ee8566b6865db7ef
LeeJungHoon <gns14585@naver.com>
2023-10-31 12:14:40 +0900

    component, 연습, 작성완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 function App() {
   return (
     <div>
-
+      <MyComponent1>
+      {/*
+      <div>
+        <h1>Hello Component</h1>
+      </div>
+      */}
+      </MyComponent1>
+      <MyComponent2>
+      {/*
+      <p>lorem ipsum alkdjfl sldkf</p>
+      <ul>
+        <li>lorem</li>
+        <li>ipsum</li>
+        <li>dolor</li>
+      </ul>
+      */}
+      </MyComponent2>
     </div>
   );
 }
 
+function MyComponent1() {
+  return (
+      <div>
+        <h1>Hello Component</h1>
+      </div>
+  )
+}
+function MyComponent2() {
+  return (
+      <div>
+        <p>Lorem ipsum dolor.</p>
+        <ul>
+          <li>lorem</li>
+          <li>ipsum</li>
+          <li>dolor</li>
+        </ul>
+      </div>
+  )
+}
 export default App;
