4fadb7aeddf066f917a434579b98a207c75ec274
LeeJungHoon <gns14585@naver.com>
2023-10-31 14:54:27 +0900

    jax에서 js 코드 작성은 {} 사용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+function MyComp() {
+  const address = "seoul";
+  const country = "korea";
+  const phone = "01099999999";
+  const age = 44;
+  const randomNumber = Math.random();
+  return (
+    <div>
+      <h1>age는 {age}</h1>
+      <h1>address는 {address}</h1>
+      <h1>country은 {country}</h1>
+      <h1>전화번호는 {phone}</h1>
+      <h1>난수 : {randomNumber}</h1>
+      <h1>1~10 : {Math.ceil(randomNumber * 10)}</h1>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-
+      <MyComp />
     </div>
   );
 }
