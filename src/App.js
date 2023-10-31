0708bbdcd80c0026603f253e735eaec741d63b22
LeeJungHoon <gns14585@naver.com>
2023-10-31 17:30:39 +0900

    주사위번호 연습

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 function App() {
-  const num1 = Math.ceil(Math.random() * 6);
-  const num2 = Math.ceil(Math.random() * 6);
+  const num1 = Math.ceil(Math.random() * 5);
+  const num2 = Math.ceil(Math.random() * 5);
   let message;
+
+  if (num1 === num2) {
+    message = "당첨"
+  } else {
+    message = "당첨안됨"
+  }
   // 두 수를 출력하고
   // 두 수가 같으면
   // 당첨이 출력되게
   // 두 수가 다르면
   // 다시 던져보세요
 
-  // 2번째 방법
-  if (num1 === num2) {
-    message = <h1>당첨</h1>;
-  } else {
-    message = <h1>다시 던져보세요</h1>;
-  }
   return (
     <div>
-      <h1>{num1}</h1>
-      <h1>{num2}</h1>
-      {/* 첫번째 방법 */}
-      <h1>{num1 == num2 ? "당첨" : "다시 던져보세요"}</h1>
-
-      <hr />
-
-      {message}
+      <h1>주사위 번호 : {num1}</h1>
+      <h1>주사위 번호 : {num2}</h1>
+      <h1>결과 : {message}</h1>
     </div>
   );
 }
