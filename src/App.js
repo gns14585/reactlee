88db089f6b9a29402e00d450811ef661c2783b36
LeeJungHoon <gns14585@naver.com>
2023-10-31 15:48:50 +0900

    프로퍼티의 값은 자바스크립트 코드가 될 수 있음

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 function App() {
-  // jsx가 js 코드로 변환됨
-  // 따라서 js 키워드를 사용할 수 없음
+  const name = "흥민";
   return (
     <>
-      {/* class 속성은 className으로 작성 */}
-      <div className="header">Lorem ipsum dolor.</div>
-      <div className="error">Lorem ipsum dolor.</div>
-      {/* for 속성은 htmlFor로 작성 */}
-      <div>
-        <label htmlFor="nameInput">이름</label>
-        <input type="text" id="nameInput" />
-      </div>
-      <div>
-        <label htmlFor="ageInput"></label>
-        <input type="text" id="ageInput" />
-      </div>
+      <h1>이름은 {name}</h1>
+      <h1>이름은 {"강인"}</h1>
 
-      <div className={}>Lorem ipsum dolor.</div>
+      <div className={"error"}>Lorem ipsum dolor.</div>
+      <div className="error">Lorem ipsum dolor.</div>
     </>
   );
 }
