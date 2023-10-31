ea8b22f99e8cecd0390d931f433ef6e47f413289
LeeJungHoon <gns14585@naver.com>
2023-10-31 14:30:37 +0900

    c

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ function MyComponent1() {
     <div style={{width: "100%"}}>
       <img
         style={{width: "100%"}}
-        src="https://i.imgur.com/SuL4x4v.jpeg"
+                     src="https://i.imgur.com/SuL4x4v.jpeg"
+
         alt="경복궁"/>
     </div>
   );
