bf7fd456d077811f94eabea69253e6a5b667259e
LeeJungHoon <gns14585@naver.com>
2023-11-01 09:38:36 +0900

    props 연습

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 function App() {
   return (
     <div>
+      <MyHeader color="white" bg="black" text="내가 만든 제목" title="a" />
+      <MyHeader color="orange" bg="skyblue" text="다시 만든 제목" />
+    {/*
+    <div>
+      <h1>내가 만든 제목</h1>
+    </div>
+    */}
 
     </div>
   );
+}
+
+function MyHeader(props) {
+  return (
+    <div>
+      <h1 title="a">내가 만든 제목</h1>
+    </div>
+  )
 }
 
 export default App;
