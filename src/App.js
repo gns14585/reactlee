3d71595d16599d44c215f616aeec676787a58d14
LeeJungHoon <gns14585@naver.com>
2023-11-01 10:46:22 +0900

    props destructuring assignment 구조분해할당 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 function App() {
   return (
     <div>
+      <MyElem name="흥민" age={44} team="토트넘" style={{color: black}} />
+      <MyElem name="강인" age={33} team="파리" style={{color: black}} />
+    </div>
+  );
+}
 
+function MyElem({ name, age, team, style }) {
+  return (
+    <div>
+      <h1 {style}>
+        <ul {style}>
+          <li>이름 : {name}</li>
+          <li>소속 : {team}</li>
+          <li>나이 : {age}</li>
+        </ul>
+      </h1>
     </div>
   );
 }
