14297fdc11bfbca2e203ebe6548e15f7b5630c19
LeeJungHoon <gns14585@naver.com>
2023-11-01 11:33:48 +0900

    props 구조분해할당, 기본값 주기, 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 function App() {
   return (
     <div>
+      <MyComp m="30px" p="10px" color="gold" bg="black" text="hello react" />
+      <MyComp m="30px" p="10px" color="gold" bg="black" />
+      <MyComp m="30px" bg="black" text="hello react" />
+      <MyComp p="10px" color="gold" text="hello react" />
+    </div>
+  );
+}
 
+function MyComp({
+  m = "30px",
+  p = "10px",
+  color = "gold",
+  bg = "black",
+  text = "hello react",
+}) {
+  return (
+    <div style={{ margin: m, padding: p, color: color, backgroundColor: bg }}>
+      <h1>{text}</h1>
     </div>
   );
 }
