ad99757818101e3acc9d49d3153248fc12761348
LeeJungHoon <gns14585@naver.com>
2023-11-01 16:15:26 +0900

    css 모듈을 사용해서 클래스명 중복을 피함 연습

---
+++ src/component/MyBlock.js
@@ -0,0 +1,8 @@
+import style from "../style1/yourStyle.module.css";
+export function MyBlock() {
+  return (
+    <div>
+      <h1 className="style">Lorem ipsum dolor sit amet.</h1>
+    </div>
+  );
+}
--- src/component/MyText.js
+++ src/component/MyText.js
@@ -1,9 +1,9 @@
-import "../style1/css1.css";
+import mystyle from "../style1/css1.module.css";
 export function MyText() {
   return (
     <div>
       {/* red배경색 , white 글자색 */}
-      <h1 className="error">Lorem ipsum dolor sit amet.</h1>
+      <h1 className="mystyle">Lorem ipsum dolor sit amet.</h1>
     </div>
   );
 }
