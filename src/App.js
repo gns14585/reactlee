98121afc6d3d2d08e89f0e5523b4b3c76a86b0e8
LeeJungHoon <gns14585@naver.com>
2023-11-01 09:52:20 +0900

    props 작성, 활용연습 시작

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 function App() {
   return (
     <div>
-
+      <MyText value="property 연습" style={} />
     </div>
   );
 }
 
+function MyText(props) {
+  return (
+    <div style={props.style}>
+      <p>{props.value}</p>
+    </div>
+  )
+}
 export default App;
