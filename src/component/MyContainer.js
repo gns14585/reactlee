f778f31d106b5d5a710156012f441c1440049610
LeeJungHoon <gns14585@naver.com>
2023-11-01 14:23:51 +0900

    default export, import 연습시작

---
+++ src/component/MyContainer.js
@@ -0,0 +1,9 @@
+function MyContainer() {
+  return <div></div>;
+}
+function MyBox() {
+  return null;
+}
+export default MyBox;
+export const person = { name: "이정훈", address: "은평구" };
+export const team = { title: "코딩학원", score: "따라가는중" };
