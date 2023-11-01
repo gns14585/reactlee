8a3239c8270664be17337b67ddb84fa986316602
LeeJungHoon <gns14585@naver.com>
2023-11-01 15:27:42 +0900

    보통의 css 파일 사용하기

--- src/component/MyBox.js
+++ src/component/MyBox.js
@@ -3,7 +3,7 @@ import "../style/style1.css";
 export function MyBox() {
   return (
     <div>
-      <h1 className="boxClass">Lorem ipsum dolor sit amet.</h1>
+      <h1 className="boxClass">Lorem ipsum dolor sit amet</h1>
     </div>
   );
 }
--- src/component/MyComp.js
+++ src/component/MyComp.js
@@ -2,7 +2,7 @@ export function MyComp() {
   return (
     <div>
       <h1 style={{ color: "gold", backgroundColor: "blue" }}>
-        Lorem ipsum dolor sit amet.
+        Lorem ipsum dolor sit amet
       </h1>
     </div>
   );
