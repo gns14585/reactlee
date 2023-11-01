9d683dea52b3b36b562aa8c47d37e1dae868ca6e
LeeJungHoon <gns14585@naver.com>
2023-11-01 09:45:55 +0900

    props 작성, 활용연습 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 function App() {
   return (
     <div>
-      <MyHeader color="white" bg="black" text="내가 만든 제목" title="a" name="aa" />
-      <MyHeader color="orange" bg="skyblue" text="다시 만든 제목" title="b" name="bb" />
+      <MyHeader color="white" bg="black" text="내가 만든 제목"/>
+      <MyHeader color="orange" bg="skyblue" text="다시 만든 제목" />
     {/*
     <div>
       <h1>내가 만든 제목</h1>
@@ -15,8 +15,8 @@ function App() {
 
 function MyHeader(props) {
   return (
-    <div>
-      <h1 >내가 만든 제목</h1>
+    <div style={{color: props.color, backgroundColor: props.bg}}>
+      <h1 title={props.title}>{props.text}</h1>
     </div>
   )
 }
