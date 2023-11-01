a39f24d4b3e12a4c608a4ed783e4ac1d92e1614a
LeeJungHoon <gns14585@naver.com>
2023-11-01 11:13:33 +0900

    props destructuring assignment 구조분해할당 연습시작, 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 function App() {
   return (
     <div>
+      <MyBox
+        text="안녕하세요 중앙정보처리학원 입니다."
+        p="5px"
+        m="10px"
+        color="red"
+        bg="yellow"
+      />
+    </div>
+  );
+}
 
+function MyBox({ p, m, bg, color, text }) {
+  return (
+    <div style={{ padding: p, margin: m, color: color, backgroundColor: bg }}>
+      <p>{text}</p>
     </div>
   );
 }
