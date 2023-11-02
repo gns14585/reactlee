d0a5e0964a4ee525bde556cb87df72211023f46f
LeeJungHoon <gns14585@naver.com>
2023-11-02 15:15:24 +0900

    Select 사용

--- src/App.js
+++ src/App.js
@@ -1,61 +1,19 @@
-import {
-  Box,
-  Center,
-  Checkbox,
-  CheckboxGroup,
-  Flex,
-  FormControl,
-  FormHelperText,
-  FormLabel,
-  Input,
-  Radio,
-  RadioGroup,
-} from "@chakra-ui/react";
+import { Box, FormControl, Select } from "@chakra-ui/react";
 
 function App() {
   return (
     <div>
-      <Center>
-        <Box w={"480px"}>
-          <FormControl>
-            <FormLabel>이름</FormLabel>
-            <Input />
-            <FormHelperText>띄어쓰기 없이 입력하세요.</FormHelperText>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center m={"5px"}>
-        <Box w="480px">
-          <FormControl>
-            <FormLabel>하나만 선택하세요</FormLabel>
-            <RadioGroup>
-              <Flex gap={5}>
-                <Radio value="value1">Lorem.</Radio>
-                <Radio value="value2">Lorem.</Radio>
-                <Radio value="value3">Lorem.</Radio>
-                <Radio value="value4">Lorem.</Radio>
-              </Flex>
-            </RadioGroup>
-          </FormControl>
-        </Box>
-      </Center>
-      <Center>
+      <Box w={"480px"}>
         <FormControl>
-          <Box w={"480px"}>
-            <CheckboxGroup>
-              <Flex gap={5}>
-                <Checkbox colorScheme="orange">Lorem ipsum dolor.</Checkbox>
-                <Checkbox colorScheme="green">
-                  Blanditiis, delectus rem?
-                </Checkbox>
-                <Checkbox colorScheme="purple">
-                  Alias, consequuntur, maiores?
-                </Checkbox>
-              </Flex>
-            </CheckboxGroup>
-          </Box>
+          <Select placeholder="선택하세요." multiple h={"120px"}>
+            <option value="value&">Lorem.</option>
+            <option value="value&">Nulla.</option>
+            <option value="value&">Omnis!</option>
+            <option value="value&">Iste.</option>
+            <option value="value&">Saepe!</option>
+          </Select>
         </FormControl>
-      </Center>
+      </Box>
     </div>
   );
 }
