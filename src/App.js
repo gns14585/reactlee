d7e180e526ef7bd3f9e3ae2e656eebb18d9e5c2d
LeeJungHoon <gns14585@naver.com>
2023-11-02 11:32:32 +0900

    simplegrid 사용, 반응형웹 적용

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
 
 function App() {
+  /*
+  base: "0em", // 0px
+  sm: "30em", // ~480px. em is a relative unit and is dependant on the font size.
+  md: "48em", // ~768px
+  lg: "62em", // ~992px
+  xl: "80em", // ~1280px
+  "2xl": "96em", // ~1536px
+   */
   return (
     <>
-      {/* Flex 가로배치 */}
-      <Flex
-        w={"100%"}
-        h={"100px"}
-        bg={"gray.300"}
-        gap={3}
-        align={"center"}
-        justify={"space-around"}
-      >
-        <Box bg={"gold"}>Lorem.</Box>
-        <Box bg={"red"}>Illo!</Box>
-        <Box bg={"gray"}>Natus!</Box>
-        <Box bg={"blue"}>Saepe.</Box>
-        <Box bg={"green"}>Cumque!</Box>
-      </Flex>
+      <SimpleGrid spacing={"10px"} columns={{ base: 2, md: 3, "2xl": 6 }}>
+        <Box bg="blue.300">Lorem.</Box>
+        <Box bg="blue.300">Nulla!</Box>
+        <Box bg="blue.300">Quaerat.</Box>
+        <Box bg="blue.300">Voluptas.</Box>
+        <Box bg="blue.300">Explicabo.</Box>
+        <Box bg="blue.300">Repellat.</Box>
+        <Box bg="blue.300">Excepturi?</Box>
+        <Box bg="blue.300">Aliquam.</Box>
+        <Box bg="blue.300">Accusantium.</Box>
+        <Box bg="blue.300">Alias.</Box>
+        <Box bg="blue.300">Ut.</Box>
+        <Box bg="blue.300">Cupiditate.</Box>
+        <Box bg="blue.300">Ipsa!</Box>
+        <Box bg="blue.300">Necessitatibus.</Box>
+        <Box bg="blue.300">Debitis.</Box>
+        <Box bg="blue.300">Culpa?</Box>
+        <Box bg="blue.300">Culpa.</Box>
+        <Box bg="blue.300">Numquam.</Box>
+        <Box bg="blue.300">Accusamus!</Box>
+        <Box bg="blue.300">Natus!</Box>
+        <Box bg="blue.300">Nihil.</Box>
+        <Box bg="blue.300">Quisquam!</Box>
+        <Box bg="blue.300">Officia?</Box>
+        <Box bg="blue.300">Animi.</Box>
+        <Box bg="blue.300">Facilis?</Box>
+        <Box bg="blue.300">Culpa.</Box>
+        <Box bg="blue.300">Vel!</Box>
+        <Box bg="blue.300">Nesciunt.</Box>
+        <Box bg="blue.300">Fugiat!</Box>
+        <Box bg="blue.300">Saepe.</Box>
+      </SimpleGrid>
     </>
   );
 }
