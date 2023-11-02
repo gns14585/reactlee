ebccf7e1c66cc7c63b4b7d8e5ddfb57c2552fd9b
LeeJungHoon <gns14585@naver.com>
2023-11-02 10:23:38 +0900

    필요없는 코드 삭제함

--- src/App.js
+++ src/App.js
@@ -1,26 +1,5 @@
-import { Box } from "@chakra-ui/react";
-
 function App() {
-  return (
-    <>
-      <Box bgColor="tomato">Lorem ipsum dolor.</Box>
-      <Box bgColor="red">Lorem ipsum dolor.</Box>
-      <Box bgColor="blue">Alias, maxime, totam.</Box>
-      <Box bgColor="blue.50">Dolore dolorem, quo?</Box>
-      <Box bgColor="blue.100">Consequatur, eos recusandae.</Box>
-      <Box bgColor="blue.900">Adipisci, facere modi.</Box>
-      <Box bgColor="gray.500">Doloremque, libero, voluptatibus?</Box>
-      <Box bgColor="gray.100">Quos, ut, vero!</Box>
-      <Box bgColor="gray.800">Animi, molestiae officia.</Box>
-      <Box bgColor="yello.600" color="gray.200">
-        Cum, et eum!
-      </Box>
-      <Box bgColor="facebook.500" color="linkedin.100">
-        Accusamus assumenda, nobis.
-      </Box>
-      <Box bgColor="red.900">Lorem ipsum dolor.</Box>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
