3690b35fb486e30340d95ed00cb47f269d30d1e9
LeeJungHoon <gns14585@naver.com>
2023-11-02 14:34:25 +0900

    Button 사용

--- src/App.js
+++ src/App.js
@@ -1,36 +1,50 @@
-import { Button, useToast } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+import { ArrowRightIcon, EmailIcon } from "@chakra-ui/icons";
 
 function App() {
-  const toast = useToast();
   return (
     <>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 완료",
-            description: "게시물이 저장되었습니다.",
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="blue">Lorem.</Button>
+      <Button colorScheme="red">Lorem.</Button>
+      <Button colorScheme="yellow">Hic!</Button>
+      <Button colorScheme="orange">Dolorum.</Button>
+      <Button colorScheme="teal" size={"sm"}>
+        Distinctio?
       </Button>
-      <Button
-        onClick={() =>
-          toast({
-            title: "저장 실패",
-            description: "게시물이 저장되지 않았습니다.",
-            duration: 2000,
-            position: "top",
-            isClosable: true,
-            status: "error",
-          })
-        }
-      >
-        토스트 띄우기
+      <Button colorScheme="messenger" size={"lg"}>
+        Repellat.
       </Button>
+      <Button colorScheme="telegram" variant="outline">
+        Dolorem!
+      </Button>
+      <Button colorScheme="orange" variant={"ghost"}>
+        Quam.
+      </Button>
+      <Button variant="link">Atque!</Button>
+      <Button leftIcon={<EmailIcon />}>Odio.</Button>
+      <Button rightIcon={<ArrowRightIcon />}>Quisquam!</Button>
+      <Button isLoading>Quasi.</Button>
+      <Button isLoading loadingText="전송중">
+        Voluptas.
+      </Button>
+      <Button>Quos.</Button>
+      <Button>Rerum.</Button>
+      <Button>Officiis.</Button>
+      <Button>Officia?</Button>
+      <Button>Vel?</Button>
+      <Button>Delectus?</Button>
+      <Button>Perspiciatis.</Button>
+      <Button>Quasi!</Button>
+      <Button>Eum?</Button>
+      <Button>Qui.</Button>
+      <Button>Hic.</Button>
+      <Button>Minima.</Button>
+      <Button>Vel?</Button>
+      <Button>Cumque.</Button>
+      <Button>Consequuntur?</Button>
+      <Button>In!</Button>
+      <Button>Nam.</Button>
+      <Button>Sapiente.</Button>
     </>
   );
 }
