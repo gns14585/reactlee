c0b431519b01977e9933daf8bb764e3dc8316bb4
LeeJungHoon <gns14585@naver.com>
2023-11-03 10:21:14 +0900

    함수를 props으로 전달

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-function MyComp({ text, address, children }) {
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+function MyComp({ to, someProp }) {
+  someProp();
   return (
-    <div>
-      <p>{text}</p>
-      <p>{address}</p>
-      <p>{children}</p>
-    </div>
+    <>
+      <div>
+        <a href={to[0]}>Link</a>);
+      </div>
+      <div>
+        <Button onClick={someProp}>버튼</Button>
+      </div>
+    </>
   );
 }
 
 function App(props) {
+  function func1() {
+    console.log("func1 실행됨");
+  }
+
   return (
     <div>
-      <MyComp text="hello" address="seoul">
-        some contents, 다른 컴포넌트가 있을 수도 있음.
-      </MyComp>
+      <MyComp to={["https://www.naver.com"]} someProp={func1}></MyComp>
     </div>
   );
 }
