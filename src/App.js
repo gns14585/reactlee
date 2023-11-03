3ea7c88b8e182a7ca816bd9853ead1061a627548
LeeJungHoon <gns14585@naver.com>
2023-11-03 11:22:49 +0900

    prop으로 함수 전달하는 다양한 방법

--- src/App.js
+++ src/App.js
@@ -9,18 +9,19 @@ function App(props) {
     console.log("2번 버튼 클릭됨");
   };
 
+  // 동일한 기능을 가진 코드를 이렇게 작성하게 되면 더 간결해짐
   let handleButtonClick = (number) => {
     console.log(number + "번째 버튼 클릭됨");
   };
 
-  handleButtonClick(3);
-  handleButtonClick(4);
   return (
     <div>
       <Button onClick={handleClickButton1}>첫번째버튼</Button>
       <Button onClick={handleClickButton2}>두번째버튼</Button>
       <Button onClick={() => handleButtonClick(3)}>세번째버튼</Button>
       <Button onClick={() => handleButtonClick(4)}>네번째버튼</Button>
+      <Button onClick={() => handleButtonClick(5)}>다섯번째버튼</Button>
+      <Button onClick={() => handleButtonClick(6)}>여섯번째버튼</Button>
     </div>
   );
 }
