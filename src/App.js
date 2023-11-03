80ad6b5bef0fc2959ef5332d37f27f307adfe617
LeeJungHoon <gns14585@naver.com>
2023-11-03 15:47:22 +0900

    두개의 useState 사용 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return <></>;
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  let result = number1 + number2;
+
+  function handleNumber1(e) {
+    setNumber1(Number(e.target.value));
+  }
+
+  function handleNumber2(e) {
+    setNumber2(Number(e.target.value));
+  }
+
+  return (
+    <>
+      <input type="number" onChange={handleNumber1} /> +
+      <input type="number" onChange={handleNumber2} />= {result}
+    </>
+  );
 }
 
 export default App;
