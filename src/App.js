0cbad4d0a9a848fd87afc0fa53d2e7bf6ad81d57
LeeJungHoon <gns14585@naver.com>
2023-11-03 14:48:50 +0900

    state가 바뀌면 component가 다시 그려짐(re-render)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Input } from "@chakra-ui/react";
 
 function App(props) {
-  function handleClick(e) {
-    // 브라우저가 해야하는 기본 기능을 취소함
-    e.preventDefault();
-    console.log("다른 일을 시킴");
-  }
+  // 상태(state가 변경(이전 state와 달라짐)
+  // 되면 Component를 다시 그림 (re render)
 
-  function handleSubmit(e) {
-    // 기본 기능 취소
-    e.preventDefault();
-    console.log("다른일을 시킴");
+  // state : react가 관리하는 component의 값
+
+  // state를 얻는 방법
+  // useState();
+  // 배열을 리턴함
+  // index 0 : 현재 상태(state)
+  // index 1 : state를 변경할 수 있는 메소드
+  // const [message, setMessage] = useState("");
+
+  const [message, setMessage] = useState("");
+
+  // 상태가 바뀔 때에만 컴포넌트가 re render됨
+
+  let text = "초기 메세지";
+  function handleInputChange(e) {
+    text = e.target.value;
+    console.log("text", text);
+  }
+  function handleButtonClick(e) {
+    setMessage("바뀐 상태");
   }
   return (
     <div>
-      <a href="https://www.naver.com" onClick={handleClick}>
-        네이버
-      </a>
-      <div>
-        <form action="https://search.daum.net/search" onSubmit={handleSubmit}>
-          <input type="text" name="q" />
-          <button>검색</button>
-        </form>
-      </div>
+      <Input onChange={handleInputChange} />
+      <p>{text}</p>
+      <Button onClick={handleButtonClick}>상태 바꾸기</Button>
+      <Button onClick={() => setMessage("")}>빈 문자열로 바꾸기</Button>
+      <p>{message}</p>
     </div>
   );
 }
