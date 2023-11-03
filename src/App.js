1e18a1bdf4a51c5f0c26b8959483c5c1dda61981
LeeJungHoon <gns14585@naver.com>
2023-11-03 09:36:02 +0900

    children prop , chakra ui 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { Button } from "@chakra-ui/react";
+
+function MyLink({ to, children }) {
+  return (
+    <div>
+      <Button as="a" href={to} colorScheme="oragne">
+        {children}
+      </Button>
+    </div>
+  );
+}
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyLink to="https://www.naver.com">naver 로 </MyLink>
+      <MyLink to="https://www.daum.net">daum 으로 </MyLink>
+    </>
+  );
 }
 
 export default App;
