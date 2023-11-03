51b7ba4a682aadf3f52466930e1adc5107fb7c9a
LeeJungHoon <gns14585@naver.com>
2023-11-03 10:46:18 +0900

    arrow function을 prop으로 전달 연습

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Button } from "@chakra-ui/react";
+
 function App() {
-  return <></>;
+  function func1() {
+    console.log("first");
+  }
+  function func2() {
+    console.log("second");
+  }
+  return (
+    <>
+      <Button onClick={func1}>Lorem.</Button>
+      <Button onClick={func2}>Perferendis?</Button>
+    </>
+  );
 }
 
 export default App;
