db0093b6e2acc797abcc58f9c8dfbffd636882d7
LeeJungHoon <gns14585@naver.com>
2023-11-06 14:02:31 +0900

    배열을 상태로 쓸 경우 상태변경 시 새로운 배열로 복사해서 사용해야함

--- src/App.js
+++ src/App.js
@@ -6,19 +6,14 @@ function App(props) {
   const [text, setText] = useState("");
 
   function handleButtonClick() {
-    // 직전 상태 배열을 조작하면 안되고
-    // coffees.push(text);
-
-    // 새 배열을 만들어서 set state 해야함
-    const newCoffees = [...coffees];
-
-    newCoffees.push(text);
-    setCoffees(newCoffees);
+    const newCoffee = [...coffees];
+    newCoffee.push(text);
+    setCoffees(newCoffee);
   }
 
   return (
     <div>
-      <Input value={text} onChange={(e) => setText(e.target.value)} />
+      <Input value={text} onChange={(e, index) => setText(e.target.value)} />
       <Button onClick={handleButtonClick}>추가</Button>
 
       <List>
