822b56c68a25be2e4c92fd41e7c9df58f2759bc5
LeeJungHoon <gns14585@naver.com>
2023-11-06 10:17:55 +0900

    state 사용 예제

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import React, { useState } from "react";
-import { Box, Input, Text } from "@chakra-ui/react";
+import { Box, Button, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [address, setAddress] = useState("");
-  function handleChangeName(e) {
-    setUsername(e.target.value);
-  }
-  function handleChangeEmail(e) {
-    setEmail(e.target.value);
-  }
+  const [number, setNumber] = useState(0);
+  console.log(number);
+
+  const index = number & 3;
 
   return (
     <div>
-      <Box>
-        <Input type="text" value={username} onChange={handleChangeName} />
+      <Button onClick={(e) => setNumber(number + 1)}>다음</Button>
+      <Box borderWidth={"3px"} borderColor={index == 0 ? "blue" : "black"}>
+        <Text>message1</Text>
       </Box>
-      <Box>
-        <Input type="text" value={email} onChange={handleChangeEmail} />
+      <Box borderWidth={"3px"} borderColor={index == 1 ? "blue" : "black"}>
+        <Text>message2</Text>
       </Box>
-      <Text>
-        {username}의 email은 {email} 입니다. 주소는 {address} 입니다.
-      </Text>
-
-      <Box>
-        <Input
-          type="text"
-          value={address}
-          onChange={(e) => setAddress(e.target.value)}
-        />
+      <Box borderWidth={"3px"} borderColor={index == 2 ? "blue" : "black"}>
+        <Text>message3</Text>
       </Box>
     </div>
   );
