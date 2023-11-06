f0a8fe8402d555c037a1930c3dcb4a9fa52b2114
LeeJungHoon <gns14585@naver.com>
2023-11-06 11:54:32 +0900

    배열을 jsx에서 사용하기

--- src/App.js
+++ src/App.js
@@ -1,35 +1,25 @@
-import React, { useState } from "react";
-import { Box, Button, Text } from "@chakra-ui/react";
+import React from "react";
+import { List, ListItem } from "@chakra-ui/react";
 
 function App(props) {
-  // set... 메소드로 상태 변경할 수 있음
-  // 상태가 같은지 아닌지 판단해서 re-render 결정함
+  const arr = ["pizza", "돈까스", "햄버거", "김치찌개"];
+  const arr2 = ["손흥민", "이강인", "김민재"];
+  const arr3 = ["라떼", "에스프레소", "카푸치노"];
 
-  const [number, setNumber] = useState(0);
-  const [numberObject, setNumberObject] = useState({ number: 0 });
-
-  function handleNumberObjectChange() {
-    // 이렇게 사용하면 안됨
-    // numberObject.number = numberObject.number + 1;
-    // setNumberObject(numberObject);
-
-    // 복사해서 사용해야함
-    const newNumberObject = { ...numberObject };
-    newNumberObject.number = newNumberObject.number + 1;
-    setNumberObject(newNumberObject);
-  }
-
+  const listItems = arr.map((item) => <ListItem>{item}</ListItem>);
   return (
     <div>
-      <Box>
-        <Button onClick={() => setNumber(number + 1)}>number 변경</Button>
-        <Text>{number}</Text>
-      </Box>
-
-      <Box>
-        <Button onClick={handleNumberObjectChange}>number 객체 변경</Button>
-        <Text>{numberObject.number}</Text>
-      </Box>
+      <List mb={3}>{listItems}</List>
+      <List mb={3}>
+        {arr2.map((item2) => (
+          <ListItem>{item2}</ListItem>
+        ))}
+      </List>
+      <List mb={3}>
+        {arr3.map((item3) => (
+          <ListItem>{item3}</ListItem>
+        ))}
+      </List>
     </div>
   );
 }
