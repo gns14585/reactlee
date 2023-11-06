5077d66e3167986f8b4a94f223fdbc6864680b8b
LeeJungHoon <gns14585@naver.com>
2023-11-06 09:53:06 +0900

    state 사용 예제 연습 완료2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
-import { Button } from "@chakra-ui/react";
+import { Box, Input, Text } from "@chakra-ui/react";
 
 function App(props) {
-  const [text, setText] = useState("hello");
-
-  console.log(text);
-
-  function handleButtonClick(e) {
-    // text 라는 state 값 변경
-    setText("greeting");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  function handleChangeName(e) {
+    setUsername(e.target.value);
+  }
+  function handleChangeEmail(e) {
+    setEmail(e.target.value);
   }
 
   return (
     <div>
-      <Button onClick={handleButtonClick}>상태 변경!</Button>
-      <p>{text}</p>
+      <Box>
+        <Input type="text" value={username} onChange={handleChangeName} />
+      </Box>
+      <Box>
+        <Input type="text" value={email} onChange={handleChangeEmail} />
+      </Box>
+      <Text>
+        {username}의 email은 {email} 입니다. 주소는 {address} 입니다.
+      </Text>
+
+      <Box>
+        <Input
+          type="text"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+      </Box>
     </div>
   );
 }
