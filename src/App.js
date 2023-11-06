217f268d45eb64220d0fbc90b118a18f5e86f284
LeeJungHoon <gns14585@naver.com>
2023-11-06 10:26:48 +0900

    hook 사용 조건 설명

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React, { useState } from "react";
-import { Box, Button, Text } from "@chakra-ui/react";
+import React from "react";
 
 function App(props) {
-  const [number, setNumber] = useState(0);
-  console.log(number);
+  // HOOK : use 로 시작하는 함수
+  // 컴포넌트 상단에 작성해야함
+  // 조건문이나, 반복문, 중첩된 함수 안에 작성하면 안됨
 
-  const index = number & 3;
+  if (true) {
+    // hook 호출 코드 작성 불가
+  }
 
-  return (
-    <div>
-      <Button onClick={(e) => setNumber(number + 1)}>다음</Button>
-      <Box borderWidth={"3px"} borderColor={index == 0 ? "blue" : "black"}>
-        <Text>message1</Text>
-      </Box>
-      <Box borderWidth={"3px"} borderColor={index == 1 ? "blue" : "black"}>
-        <Text>message2</Text>
-      </Box>
-      <Box borderWidth={"3px"} borderColor={index == 2 ? "blue" : "black"}>
-        <Text>message3</Text>
-      </Box>
-    </div>
-  );
+  for (let i = 0; i < 3; i++) {
+    // hook 호출 코드 작성 불가
+  }
+
+  function handle() {
+    // hook 호출 코드 작성 불가
+  }
+
+  return <div></div>;
 }
 
 export default App;
