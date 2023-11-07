6748c75717c1a34af5c0a08db667b898a1f95a00
LeeJungHoon <gns14585@naver.com>
2023-11-07 14:14:41 +0900

    context value에 {state, setState}를 넣기

--- src/App.js
+++ src/App.js
@@ -1,58 +1,35 @@
 import React, { createContext, useContext, useState } from "react";
-import {
-  Box,
-  Button,
-  Input,
-  ListItem,
-  Text,
-  UnorderedList,
-} from "@chakra-ui/react";
-
-function CComp() {
-  const message = useContext(MessageContext);
-  return <Text>{message}</Text>;
-}
-
-function BComp() {
-  return <CComp />;
-}
+import { Button, Text } from "@chakra-ui/react";
 
 function AComp() {
-  return <BComp />;
-}
-
-function App(props) {
-  const [message, setMessage] = useState("");
-  const [items, setItems] = useState([]);
-
-  function handleButtonClick() {
-    const nextItem = [...items];
-    nextItem.push(message);
-    setItems(nextItem);
-  }
-
+  // 3. context 사용하기 : useContext(Context);
+  const { setMessage } = useContext(MessageContext);
   return (
-    <>
-      <Box>
-        <Input onChange={(e) => setMessage(e.target.value)} />
-        <MessageContext.Provider value={message}>
-          <AComp />
-        </MessageContext.Provider>
-        <Button onClick={handleButtonClick}>추가</Button>
-      </Box>
-
-      {/* 리스트 추가 */}
-      <Box>
-        <UnorderedList>
-          {items.map((item, index) => (
-            <ListItem key={index}>{item}</ListItem>
-          ))}
-        </UnorderedList>
-      </Box>
-    </>
+    <Button onClick={() => setMessage("변경된메세지")}>메세지 변경하기</Button>
   );
 }
 
+function BComp() {
+  // 3. context 사용하기 : useContext(Context);
+  const { message } = useContext(MessageContext);
+  return <Text>{message}</Text>;
+}
+
+function App(props) {
+  const [message, setMessage] = useState("초기 상태 메세지");
+
+  return (
+    <div>
+      {/* 2. context에 state 넣기 : <Context.Provider value={state}></Context.Provider> */}
+      {/* value 안에 있는 state명과, setstate명의 값들이 동일하면 생략 가능 */}
+      <MessageContext.Provider value={{ message, setMessage }}>
+        <AComp />
+        <BComp />
+      </MessageContext.Provider>
+    </div>
+  );
+}
+// 1. context 만들기 : createContext(null);
 const MessageContext = createContext(null);
 
 export default App;
