5295a6936b604232915b5967badd2437747a3294
LeeJungHoon <gns14585@naver.com>
2023-11-07 14:53:05 +0900

    component 밖의 값을 가능한 변경하지 않도록 할 것

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import React, { createContext, useState } from "react";
-import { AComp } from "./AComp";
-import { BComp } from "./BComp";
+import React, { useState } from "react";
+import { Button } from "@chakra-ui/react";
 
-function App(props) {
-  const [message, setMessage] = useState("초기 상태 메세지");
+let out = 0;
+function MyComp() {
+  const [num, setNum] = useState(0);
+  let a = 3;
 
+  out++;
+
+  console.log("a", a);
+  console.log("num", num);
+  console.log("out", out);
   return (
     <div>
-      {/* 2. context에 state 넣기 : <Context.Provider value={state}></Context.Provider> */}
-      {/* value 안에 있는 state명과, setstate명의 값들이 동일하면 생략 가능 */}
-      <MessageContext.Provider value={{ message, setMessage }}>
-        <AComp />
-        <BComp />
-      </MessageContext.Provider>
+      <Button onClick={() => setNum(num + 1)}>증가</Button>
+      <p>{num}</p>
     </div>
   );
 }
-// 1. context 만들기 : createContext(null);
-export const MessageContext = createContext(null);
+
+function App(props) {
+  return (
+    <div>
+      <MyComp />
+    </div>
+  );
+}
 
 export default App;
