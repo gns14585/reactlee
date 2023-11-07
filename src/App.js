f123cab20866c9623c955b50376b2cf31fb87954
LeeJungHoon <gns14585@naver.com>
2023-11-07 16:26:55 +0900

    ajax get 사용 state 업데이트 연습 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App(props) {
-  return <div></div>;
+  const [message, setMessage] = useState("");
+
+  // useEffect로
+  // get요청 /api/main1/sub2
+  // 받은 값으로 message state 업데이트
+  useEffect(() => {
+    axios
+      .get("/api/main1/sub2")
+      .then((response) => response.data)
+      .then((data) => setMessage(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>{message}</h1>
+    </div>
+  );
 }
 
 export default App;
