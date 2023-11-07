42e5ee477ded25dca39a3359d48b65333741e852
LeeJungHoon <gns14585@naver.com>
2023-11-07 14:31:57 +0900

    context value에 {state, setState}를 넣어서 전달하기 연습 완료

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { createContext, useContext, useState } from "react";
+import {
+  Box,
+  Button,
+  Input,
+  ListItem,
+  Text,
+  UnorderedList,
+} from "@chakra-ui/react";
+
+function MyInput() {
+  const { setText } = useContext(TextContext);
+  return <Input onChange={(e) => setText(e.target.value)} />;
+}
+
+function MyText() {
+  const { text } = useContext(TextContext);
+  return <Text>{text}</Text>;
+}
+
+function App(props) {
+  const [text, setText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleButtonClick() {
+    const newItem = [...items];
+    newItem.push(text);
+    setItems(newItem);
+  }
+
+  return (
+    <>
+      <div>
+        <TextContext.Provider value={{ text, setText }}>
+          <MyInput></MyInput>
+          <MyText></MyText>
+        </TextContext.Provider>
+        <Button onClick={handleButtonClick}>추가</Button>
+      </div>
+
+      <Box>
+        <UnorderedList>
+          {items.map((item, index) => (
+            <ListItem key={index}>{item}</ListItem>
+          ))}
+        </UnorderedList>
+      </Box>
+    </>
+  );
+}
+
+const TextContext = createContext(null);
+
+export default App;
