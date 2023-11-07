7de5d63f61989e834ea08b033a4018eeea45f631
LeeJungHoon <gns14585@naver.com>
2023-11-07 14:43:38 +0900

    Context 를 export해서 다른 컴포넌트에서 import 해서 사용

--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import React, { createContext, useContext, useState } from "react";
-import {
-  Box,
-  Button,
-  Input,
-  ListItem,
-  Text,
-  UnorderedList,
-} from "@chakra-ui/react";
-
-function MyInput() {
-  const { setText } = useContext(TextContext);
-  return <Input onChange={(e) => setText(e.target.value)} />;
-}
-
-function MyText() {
-  const { text } = useContext(TextContext);
-  return <Text>{text}</Text>;
-}
+import React, { createContext, useState } from "react";
+import { Box, Button, ListItem, UnorderedList } from "@chakra-ui/react";
+import { MyInput } from "./component/MyInput";
+import { MyText } from "./component/MyText";
 
 function App(props) {
   const [text, setText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleButtonClick() {
-    const newItem = [...items];
-    newItem.push(text);
-    setItems(newItem);
-  }
-
-  function handleButtonRemoveClick(index) {
-    const removeItem = [...items];
-    removeItem.splice(index, 1);
-    setItems(removeItem);
-  }
+  // function handleButtonClick() {
+  //   const newItem = [...items];
+  //   newItem.push(text);
+  //   setItems(newItem);
+  // }
+  //
+  // function handleButtonRemoveClick(index) {
+  //   const removeItem = [...items];
+  //   removeItem.splice(index, 1);
+  //   setItems(removeItem);
+  // }
 
   return (
     <>
@@ -41,25 +26,25 @@ function App(props) {
           <MyInput></MyInput>
           <MyText></MyText>
         </TextContext.Provider>
-        <Button onClick={handleButtonClick}>추가</Button>
+        {/*<Button onClick={handleButtonClick}>추가</Button>*/}
       </div>
 
-      <Box>
-        <UnorderedList>
-          {items.map((item, index) => (
-            <ListItem key={index}>
-              {item}
-              <Button onClick={() => handleButtonRemoveClick(index)}>
-                지우기
-              </Button>
-            </ListItem>
-          ))}
-        </UnorderedList>
-      </Box>
+      {/*<Box>*/}
+      {/*  <UnorderedList>*/}
+      {/*    {items.map((item, index) => (*/}
+      {/*      <ListItem key={index}>*/}
+      {/*        {item}*/}
+      {/*        <Button onClick={() => handleButtonRemoveClick(index)}>*/}
+      {/*          지우기*/}
+      {/*        </Button>*/}
+      {/*      </ListItem>*/}
+      {/*    ))}*/}
+      {/*  </UnorderedList>*/}
+      {/*</Box>*/}
     </>
   );
 }
 
-const TextContext = createContext(null);
+export const TextContext = createContext(null);
 
 export default App;
